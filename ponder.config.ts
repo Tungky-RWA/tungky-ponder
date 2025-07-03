74cc32abc7014e3b713ec322a3ce7f69de2c583e
terkoizmy <dimas.aryan021@gmail.com>
2025-07-04 00:41:51 +0700

    fixing factory patern

--- ponder.config.ts
+++ ponder.config.ts
@@ -1,6 +1,7 @@
 import { createConfig, factory } from "ponder";
 import { MasterAbi } from "./abis/MasterAbi";
 import { FactoryAbi } from "./abis/FactoryAbi";
+import { parseAbiItem } from "viem";
 
 export default createConfig({
   chains: {
@@ -20,9 +21,17 @@ export default createConfig({
     Factory: {
       chain: "monadTestnet",
       abi: FactoryAbi,
-      address: process.env.CONTRACTFACTORY_ADDRESS as `0x${string}`,
+
       startBlock: 24436113,
       endBlock: 24459809,
+      address: factory({
+        // Address of the factory contract.
+        address: process.env.CONTRACTFACTORY_ADDRESS as `0x${string}`,
+        // Event from the factory contract ABI which contains the child address.
+        event: parseAbiItem("event NewPair(address poolAddress)"),
+        // Name of the event parameter containing the child address.
+        parameter: "poolAddress",
+      }),
     },
   },
 });
--- ponder.schema.ts
+++ ponder.schema.ts
@@ -12,10 +12,10 @@ export const brand = onchainTable("brand", (t) => ({
 }));
 
 export const nft = onchainTable("nft", (t) => ({
-  id: t.text().primaryKey(),
+  Id: t.text().primaryKey(),
   NameNFT: t.text(),
   ownerAddress: t.hex(),
-  NFTBrandAddress: t.hex(),
+  NftContractAddress: t.hex(),
   blockNumber: t.bigint(),
   blockTimestamp: t.bigint(),
   transactionHash: t.hex(),
@@ -24,11 +24,27 @@ export const nft = onchainTable("nft", (t) => ({
   longitude: t.bigint(),
 }));
 
-export const buyer = onchainTable("buyer", (t) => ({
-  id: t.text().primaryKey(),
-  NameNFT: t.text(),
-  NFTBrandAddress: t.hex(),
+export const nftTransfer = onchainTable("nftTransfer", (t) => ({
+  id: t.bigint().primaryKey(), // auto-increment id
+  serial: t.text(), // serial.
+  contractAddress: t.hex(),
+  tokenId: t.bigint(),
+  from: t.hex(),
+  to: t.hex(),
   blockNumber: t.bigint(),
   blockTimestamp: t.bigint(),
-  transactionHash: t.hex(),
+  transactionHash: t.text(),
+}));
+
+// 3️⃣ Relasi: Brand has many NFTs
+export const brandRelations = relations(brand, ({ many }) => ({
+  nfts: many(nft), // ← many NFT
+}));
+
+// 4️⃣ Relasi: NFT belongs to one Brand
+export const nftRelations = relations(nft, ({ one }) => ({
+  brand: one(brand, {
+    fields: [nft.NftContractAddress], // ← foreign key di nft
+    references: [brand.NftContractAddress], // ← primary key (or unique key) di brand
+  }),
 }));
